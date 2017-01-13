82900ffa6d898378d87984bffa9657f0afc1ec44
Jordan Hall <jordan.hall@rapidweb.biz>
2017-01-13 11:55:43 +0000

    Function exists check

--- src/DotEnvLoader.php
+++ src/DotEnvLoader.php
@@ -3,16 +3,20 @@
 echo '***!';
 
 if (function_exists('dotEnvLoader')) {
+
     throw new Exception('dotEnvLoaderFunction variable already exists. Ensure you do not have two copies of the dotenv-loader package.');
+    
+} else {
+
+    function dotEnvLoader() {
+
+        $path = realpath(__DIR__.'/../../../../');
+
+        $dotEnv = new Dotenv\Dotenv($path);
+
+        $dotEnv->load();
+    }
+
+    dotEnvLoader();
+
 }
-
-function dotEnvLoader() {
-
-    $path = realpath(__DIR__.'/../../../../');
-
-    $dotEnv = new Dotenv\Dotenv($path);
-
-    $dotEnv->load();
-}
-
-dotEnvLoader();
