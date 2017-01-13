b29b3319800e8492f316f8527c281aed678afe4f
Jordan Hall <jordan.hall@rapidweb.biz>
2017-01-13 12:03:35 +0000

    Test files

---
+++ test/index.php
@@ -0,0 +1,29 @@
+<?php
+
+require 'vendor/autoload.php';
+
+echo ucfirst(getenv('MICE_QUANTITY')).' '.getenv('MICE_STATUS').' mice. ';
+echo ucfirst(getenv('MICE_QUANTITY')).' '.getenv('MICE_STATUS').' mice. ';
+
+echo '<br/>';
+
+echo 'See how they '.getenv('MICE_SPEED').'. ';
+echo 'See how they '.getenv('MICE_SPEED').'. ';
+
+echo '<br/>';
+
+echo 'They all ran after the farmer\'s wife,';
+
+echo '<br/>';
+
+echo 'Who cut off their tails with a '.getenv('CUTTING_TOOL').',';
+
+echo '<br/>';
+
+echo 'Did you ever see such a sight in your life,';
+
+echo '<br/>';
+
+echo 'As '.getenv('MICE_QUANTITY').' '.getenv('MICE_STATUS').' mice?';
+
+echo '<br/>';
